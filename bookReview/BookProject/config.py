72631b12fecbfcb9ec5f90dbabfc64e201f4f005
lenhat509 <60248463+lenhat509@users.noreply.github.com>
2020-08-22 12:44:39 -0700

    Update config.py

--- bookReview/BookProject/config.py
+++ bookReview/BookProject/config.py
@@ -1,5 +1,5 @@
 import json
-file = open('C:\\Users\\Le Nhat\\VSC project\\bookReview\\BookProject\\config.json', "r")
+file = open('config.json', "r")
 dic = json.load(file)
 class Config:
     SQLALCHEMY_DATABASE_URI= dic.get("SQLALCHEMY_DATABASE_URI")
